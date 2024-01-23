b3b98a7a090f424e999e0210518659b884c4eae8
Lukas7u1ehner <lukas.zulehner@outlook.com>
2024-01-24 00:02:08 +0100

    mailservice changes

--- Database/testing.sql
+++ Database/testing.sql
@@ -4,4 +4,6 @@ SELECT * FROM users; --check if the data is inserted correctly in the users tabl
 
 SELECT * FROM offers; --check if the data is inserted correctly in the offers table
 
+SELECT * FROM sent_offers; --check if the data is inserted correctly in the sent_offers table
+
 SELECT * FROM search_parameters; --check if the data is inserted correctly in the search_parameters table
